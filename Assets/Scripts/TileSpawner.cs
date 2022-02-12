3d5c723e5816b736a2ceaaa3d8e6fd03cdb9dfa5
Steven Zaworski <szaworski@protonmail.com>
2022-02-12 00:58:05 -0600

    Starting path building prototype

--- Assets/Scripts/TileSpawner.cs
+++ Assets/Scripts/TileSpawner.cs
@@ -3,20 +3,32 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class TileSpawner : MonoBehaviour
+public class TileSpawner : TileTypes
 {
     public int curXpos = 0;
     public int curYpos = 0;
     public int shiftAmtXpos;
     public int shiftAmtYpos;
+    public int numOfTimesPlaced;
     public bool checkTopOverlap;
     public bool checkBottomOverlap;
     public bool checkLeftOverlap;
     public bool checkRightOverlap;
+    public string tileName;
+    public string curTileName;
+
+    public bool tile1IsValid;
+    public bool tile2IsValid;
+    public bool tile3IsValid;
+    public bool tile4IsValid;
+    public bool tile5IsValid;
+    public bool tile6IsValid;
+    List<string> validTiles;
 
     void Start()
     {
         PlaceStartingTile();
+        numOfTimesPlaced = 0;
     }
 
     void Update()
@@ -27,6 +39,7 @@ public class TileSpawner : MonoBehaviour
     public void PlaceStartingTile()
     {
         //Fetch the starting tile
+        //GameObject referenceStartTile = (GameObject)Instantiate(Resources.Load("BasicTiles/Tile3"));
         GameObject referenceStartTile = (GameObject)Instantiate(Resources.Load("StartingTile"));
 
         //Place the starting tile
@@ -41,10 +54,17 @@ public class TileSpawner : MonoBehaviour
     {
         if (Input.GetMouseButtonDown(0))
         {
-            //shiftAmtYpos += 1;
-            shiftAmtXpos += 1;
+            if (numOfTimesPlaced < 1)
+            {
+                tileName = "StartingTile";
+                shiftAmtYpos += 1;
+               //shiftAmtXpos += 1;
+            }
 
-            String tileName = "StartingTile";
+            else
+            {
+                FetchNewTile();
+            }
 
             //Fetch the new tile
             GameObject referenceStartTile = (GameObject)Instantiate(Resources.Load(tileName));
@@ -55,13 +75,113 @@ public class TileSpawner : MonoBehaviour
 
             //Destroy the temporary reference object
             Destroy(referenceStartTile);
-            CheckTileOverlap();
+
+            //increment numOfTimesPlaced
+            numOfTimesPlaced++;
         }
     }
 
     public void FetchNewTile()
     {
+        CheckTileOverlap();
+        CheckForValidTiles();
+        GetValidTiles();
 
+    }
+
+    public void GetValidTiles()
+    {
+        validTiles = new List<string>();
+
+        if (tile1IsValid)
+        {
+            validTiles.Add(basicTiles[0]);
+        }
+
+        if (tile2IsValid)
+        {
+            validTiles.Add(basicTiles[1]);
+        }
+
+        if (tile3IsValid)
+        {
+            validTiles.Add(basicTiles[2]);
+        }
+
+        if (tile4IsValid)
+        {
+            validTiles.Add(basicTiles[3]);
+        }
+
+        if (tile5IsValid)
+        {
+            validTiles.Add(basicTiles[4]);
+        }
+
+        if (tile6IsValid)
+        {
+            validTiles.Add(basicTiles[5]);
+        }
+    }
+
+public void CheckForValidTiles()
+    {
+        if (checkTopOverlap)
+        {
+            tile1IsValid = false;
+            tile3IsValid = false;
+            tile4IsValid = false;
+        }
+
+        else
+        {
+            tile1IsValid = true;
+            tile3IsValid = true;
+            tile4IsValid = true;
+        }
+
+        if (checkBottomOverlap)
+        {
+            tile1IsValid = false;
+            tile5IsValid = false;
+            tile6IsValid = false;
+        }
+
+        else
+        {
+            tile1IsValid = true;
+            tile5IsValid = true;
+            tile6IsValid = true;
+        }
+
+        if (checkLeftOverlap)
+        {
+            tile2IsValid = false;
+            tile3IsValid = false;
+            tile6IsValid = false;
+        }
+
+        else
+        {
+            tile2IsValid = true;
+            tile3IsValid = true;
+            tile6IsValid = true;
+        }
+
+
+        if (checkRightOverlap)
+        {
+            tile2IsValid = false;
+            tile4IsValid = false;
+            tile5IsValid = false;
+        }
+
+        else
+        {
+            tile2IsValid = true;
+            tile4IsValid = true;
+            tile5IsValid = true;
+        }
     }
 
     public void CheckTileOverlap()
---
+++ Assets/Scripts/TileTypes.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TileTypes : MonoBehaviour
+{
+    public string[] basicTiles = { "Tile1", "Tile2", "Tile3", "Tile4", "Tile5", "Tile6" };
+
+}
