58aa4ab340564b4138c5c5e7a2abdf4ac61ec71e
Steven Zaworski <szaworski@protonmail.com>
2022-02-21 21:41:43 -0600

    Working on tower grid

---
+++ Assets/Scripts/Tower/TowerGrid.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TowerGrid : MonoBehaviour
+{
+    //For whatever reason after changing the collider from a BoxCollider2D to a normal box collider, now it behaves how I want it to.
+    public SpriteRenderer sprite;
+    void Awake()
+    {
+        sprite = this.gameObject.GetComponent<SpriteRenderer>();
+        sprite.enabled = false;
+    }
+
+    void OnMouseOver()
+    {
+        //Reveal the grid sprite on mouse over
+        sprite.enabled = true;
+        //Debug.Log("Is hovering");
+    }
+
+    void OnMouseExit()
+    {
+        //Hide the grid sprite when on mouse exit 
+        sprite.enabled = false;
+        //Debug.Log("Not hovering");
+    }
+
+    void OnMouseDown()
+    {
+        //Need options for building towers once the grid space has been clicked
+        Debug.Log("Clicked on grid item");
+    }
+}
