749868629d4570447b3ef5bf13b6a0231179d32a
Steven Zaworski <szaworski@protonmail.com>
2022-03-05 02:01:50 -0600

    A few sprite updates

---
+++ Assets/Scripts/Tiles/CardLists.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CardsLists : MonoBehaviour
+{
+    [System.NonSerialized] public List<string> tier1TileCards = new List<string> { "Forrest", "Graveyard", "River", "Mountain", "Swamp" };
+    //[System.NonSerialized] public List<string> tier2TileCards = new List<string> { "Forrest", "Graveyard", "River", "Mountain", "Swamp" };
+    //[System.NonSerialized] public List<string> tier3TileCards = new List<string> { "Forrest", "Graveyard", "River", "Mountain", "Swamp" };
+
+    [System.NonSerialized] public List<string> forrestMonsterCards = new List<string> { "Wolf", "Goblin", "Robber", "Owl" };
+    //[System.NonSerialized] public List<string> graveyardMonsterCards = new List<string> { "Zombie", "Skeleton", "Ghost", "Bat" };
+    //[System.NonSerialized] public List<string> riverMonsterCards = new List<string> { "Forrest", "Graveyard", "River", "Mountain" };
+    //[System.NonSerialized] public List<string> mountainMonsterCards = new List<string> { "Forrest", "Graveyard", "River", "Mountain" };
+    //[System.NonSerialized] public List<string> swampMonsterCards = new List<string> { "Forrest", "Graveyard", "River", "Mountain" };
+}
--- Assets/Scripts/Tiles/TileCards.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class CardsLists : MonoBehaviour
-{
-    [System.NonSerialized] public List<string> tier1TileCards = new List<string> { "Forrest", "Graveyard", "River", "Mountain", "Swamp" }; 
-
-}
