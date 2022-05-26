4fcbedeb30b6c0499840c4d2095ef69a9a661899
Steven Zaworski <szaworski@protonmail.com>
2022-05-26 00:07:03 -0500

    Progress on MouseCursor script

--- Assets/Scripts/Player/MouseCursor.cs
+++ Assets/Scripts/Player/MouseCursor.cs
@@ -4,16 +4,31 @@ using UnityEngine;
 
 public class MouseCursor : MonoBehaviour
 {
-    public Texture2D cursorImg;
+    public static string currentWeapon;
+    [SerializeField] private Texture2D cursorImage;
+    [SerializeField] private Texture2D[] weaponImages;
 
     void Awake()
     {
-        Cursor.SetCursor(cursorImg, Vector2.zero, CursorMode.ForceSoftware);
+        Cursor.SetCursor(cursorImage, Vector2.zero, CursorMode.ForceSoftware);
     }
 
 
     void Update()
     {
-        
+        SwapMouseCursor();
+    }
+
+    public void SwapMouseCursor()
+    {
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            Cursor.SetCursor(cursorImage, Vector2.zero, CursorMode.ForceSoftware);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            Cursor.SetCursor(weaponImages[0], Vector2.zero, CursorMode.ForceSoftware);
+        }
     }
 }
---
+++ Assets/Scripts/Player/PlayerWeapons.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerWeapons : MonoBehaviour
+{
+    [SerializeField] private string weaponName;
+    [SerializeField] private Texture2D weaponImage;
+}
