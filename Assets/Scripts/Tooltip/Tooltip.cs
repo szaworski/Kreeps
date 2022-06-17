d2d16933d4a9e0cd5f5f47cf939388c8bddceab4
Steven Zaworski <szaworski@protonmail.com>
2022-06-17 17:57:48 -0500

    Adding Tooltip functionality

---
+++ Assets/Scripts/Tooltip/Tooltip.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tooltip : MonoBehaviour
+{
+    public string text;
+
+    private void OnMouseOver()
+    {
+        if (!GlobalVars.IsHoveringOverUiCard)
+        {
+            TooltipManager.tooltipInstance.SetAndShowTooltip(text);
+        }
+    }
+
+    private void OnMouseExit()
+    {
+        TooltipManager.tooltipInstance.HideTooltip();
+    }
+}
---
+++ Assets/Scripts/Tooltip/TooltipManager.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class TooltipManager : MonoBehaviour
+{
+    public static TooltipManager tooltipInstance;
+    public TextMeshProUGUI textObj;
+
+    private void Awake()
+    {
+        if (tooltipInstance != null && tooltipInstance != this)
+        {
+            Destroy(this.gameObject);
+        }
+
+        else
+        {
+            tooltipInstance = this;
+        }
+    }
+
+    void Start()
+    {
+        gameObject.SetActive(false);
+    }
+
+    void Update()
+    {
+        transform.position = Input.mousePosition;
+        //mouseScreenPosition = Input.mousePosition;
+        //transform.position =  camera.ScreenToWorldPoint(new Vector3(mouseScreenPosition.x, mouseScreenPosition.y, camera.nearClipPlane));
+    }
+
+    public void SetAndShowTooltip(string text)
+    {
+        gameObject.SetActive(true);
+        textObj.text = text;
+    }
+
+    public void HideTooltip()
+    {
+        gameObject.SetActive(false);
+        textObj.text = string.Empty;
+    }
+}
