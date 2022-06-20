0d1995cc3fb6645cfdcd0b7691c396d2b2cb910e
Steven Zaworski <szaworski@protonmail.com>
2022-06-20 12:01:46 -0500

    Created Tier 2 weapon prefabs

--- Assets/Scripts/Player/MouseCursor.cs
+++ Assets/Scripts/Player/MouseCursor.cs
@@ -56,42 +56,54 @@ public class MouseCursor : MonoBehaviour
             {
                 //Lvl 1 Weapons
                 case "Dagger":
-                    ChangeWeaponStats(3, 1, 0.5f, 0.1f, "Neutral", 0, true);
-                    weaponImageIndex = 0;
-                    playerHudScript.GetSetweaponHudImage.sprite = playerHudScript.GetWeaponHudImagesList[1];
+                    ChangeWeaponStats(3, 1, 0.5f, 0.1f, "Neutral", 0, true, 0);
                     break;
 
                 case "ShortSword":
-                    ChangeWeaponStats(8, 1, 1f, 0.1f, "Neutral", 0, true);
-                    weaponImageIndex = 1;
-                    playerHudScript.GetSetweaponHudImage.sprite = playerHudScript.GetWeaponHudImagesList[2];
+                    ChangeWeaponStats(8, 1, 1f, 0.1f, "Neutral", 0, true, 1);
                     break;
 
                 case "LongSword":
-                    ChangeWeaponStats(10, 0, 2f, 0.2f, "Neutral", 0, true);
-                    weaponImageIndex = 2;
-                    playerHudScript.GetSetweaponHudImage.sprite = playerHudScript.GetWeaponHudImagesList[3];
+                    ChangeWeaponStats(10, 0, 2f, 0.2f, "Neutral", 0, true, 2);
                     break;
 
                 case "Spear":
-                    ChangeWeaponStats(15, 1, 1.25f, 0.1f, "Neutral", 0, true);
-                    weaponImageIndex = 3;
-                    playerHudScript.GetSetweaponHudImage.sprite = playerHudScript.GetWeaponHudImagesList[4];
+                    ChangeWeaponStats(15, 1, 1.25f, 0.1f, "Neutral", 0, true, 3);
                     break;
 
                 case "HandAxe":
-                    ChangeWeaponStats(8, 1, 0.6f, 0.1f, "Neutral", 0, true);
-                    weaponImageIndex = 4;
-                    playerHudScript.GetSetweaponHudImage.sprite = playerHudScript.GetWeaponHudImagesList[5];
+                    ChangeWeaponStats(8, 1, 0.6f, 0.1f, "Neutral", 0, true, 4);
                     break;
 
                 case "Mace":
-                    ChangeWeaponStats(12, 1, 1f, 0.1f, "Neutral", 0, true);
-                    weaponImageIndex = 5;
-                    playerHudScript.GetSetweaponHudImage.sprite = playerHudScript.GetWeaponHudImagesList[6];
+                    ChangeWeaponStats(12, 1, 1f, 0.1f, "Neutral", 0, true, 5);
                     break;
 
-                    //Lvl 2 Weapons
+                //Lvl 2 Weapons
+                case "SilverShortSword":
+                    ChangeWeaponStats(16, 1, 0.9f, 0.15f, "Neutral", 0, true, 6);
+                    break;
+                case "CharredDagger":
+                    ChangeWeaponStats(10, 1, 0.5f, 0.15f, "Fire", 0, true, 7);
+                    break;
+                case "FrostWand":
+                    ChangeWeaponStats(8, 0, 1.25f, 0.2f, "Ice", 0.05f, false, 8);
+                    break;
+                case "JoltSabre":
+                    ChangeWeaponStats(20, 1, 1f, 0.15f, "Thunder", 0, true, 9);
+                    break;
+                case "DivineHammer":
+                    ChangeWeaponStats(30, 1, 1.25f, 0.15f, "Holy", 0, true, 10);
+                    break;
+                case "Katana":
+                    ChangeWeaponStats(20, 1, 0.7f, 0.15f, "Swift", 0, true, 11);
+                    break;
+                case "AstralSpear":
+                    ChangeWeaponStats(40, 1, 1.5f, 0.15f, "Cosmic", 0, true, 12);
+                    break;
+
+                //Lvl 3 Weapons
+
             }
 
             if (GlobalVars.weaponIsSelected)
@@ -102,7 +114,7 @@ public class MouseCursor : MonoBehaviour
     }
 
 
-    public void ChangeWeaponStats(float damage, float projectileSpeed, float attackSpeed, float attackRange, string damageType, float slowAmt, bool useSlashAnim)
+    public void ChangeWeaponStats(float damage, float projectileSpeed, float attackSpeed, float attackRange, string damageType, float slowAmt, bool useSlashAnim, int weaponIndex)
     {
         weaponScript.startingDamage = damage;
         weaponScript.bonusDamage = 0;
@@ -112,5 +124,7 @@ public class MouseCursor : MonoBehaviour
         weaponScript.damageType = damageType;
         weaponScript.slowAmt = slowAmt;
         GlobalVars.useSlashAnim = useSlashAnim;
+        weaponImageIndex = weaponIndex;
+        playerHudScript.GetSetweaponHudImage.sprite = playerHudScript.GetWeaponHudImagesList[weaponIndex + 1];
     }
 }
