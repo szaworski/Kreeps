4314e81d0c8c042216922fbb21326bc8f4f3c4c8
Steven Zaworski <szaworski@protonmail.com>
2022-06-28 17:57:10 -0500

    Making changes to the Main menu screen

--- Assets/Scripts/Tiles/WaypointManager.cs
+++ Assets/Scripts/Tiles/WaypointManager.cs
@@ -5,14 +5,9 @@ using UnityEngine;
 public class WaypointManager : MonoBehaviour
 {
     public int waypointNum;
-    GameObject tileManager;
-    TileSpawner tileSpawnerScript;
 
     void Awake()
     {
-        tileManager = GameObject.Find("TileManager");
-        tileSpawnerScript = tileManager.GetComponent<TileSpawner>();
-
         waypointNum = GlobalVars.tileCounters["numOfTimesPlaced"];
     }
 }
