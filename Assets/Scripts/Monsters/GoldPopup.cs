5b58e07cf54278b84c9eaf0f01c443c9d4cfc390
Steven Zaworski <szaworski@protonmail.com>
2023-02-27 19:05:17 -0600

    Changes to Holy/Thunder tower stats

--- Assets/Scripts/Monsters/GoldPopup.cs
+++ Assets/Scripts/Monsters/GoldPopup.cs
@@ -7,6 +7,6 @@ public class GoldPopup : MonoBehaviour
     void Update()
     {
         this.transform.position = Vector3.MoveTowards(this.transform.position, new Vector3(this.transform.position.x, this.transform.position.y + 0.05f, 0), 0.5f * Time.deltaTime);
-        Destroy(gameObject, 0.3f);
+        Destroy(gameObject, 0.25f);
     }
 }
